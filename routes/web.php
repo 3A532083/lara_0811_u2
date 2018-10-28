82e900002ff15b9c3126f35930a8205cd2e6426f
3A532083 <luckyday604@gm.student.ncutedu.tw>
2018-10-29 00:25:15 +0800

    練習5.2:設定另一個 Route 以群組包起來設定 prefix

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('welcome', function () {
     return view('welcome');
@@ -23,4 +14,10 @@ Route::get('say/{name?}', ['as' => 'hello.index', function($name = 'Everybody') 
 
 Route::get('dashboard', function() {
     return 'dashboard';
+});
+
+Route::group(['prefix'=>'admin'], function () {
+    Route::get('dashboard', function () {
+        return 'admin dashboard';
+    });
 });
